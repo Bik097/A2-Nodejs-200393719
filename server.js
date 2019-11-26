e99f2b43aab81eed8ad97449b4a93175b3eb7397
Bik097 <bjs241997@gmail.com>
2019-11-26 18:13:56 -0500

    student controller test

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require('./models/db');
 const express = require('express');
 var app = express();
 
+const studentController = require('./controllers/studentController');
+
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
 });
+
+app.use('/student', studentController);
