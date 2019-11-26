f63564267cb6a53a72c30506e557608467d1a4ea
Bik097 <bjs241997@gmail.com>
2019-11-26 18:03:33 -0500

    first commit

---
+++ models/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+var url = 'mongodb+srv://bik:Test@12345@cluster0-k4bur.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true 
+}).then(value => console.log('Chal Pda'));
+
+//mongoose.connect('mongodb+srv://bik:Test@12345@cluster0-k4bur.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+  //  if (!err) { console.log('MongoDB Connection Succeeded.') }
+    //else { console.log('Error in DB connection : ' + err) }
+//});
+
+
+require('./student.model');
