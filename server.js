74ca41afb30f15ac359b18a9daf6ff3d2b13a25a
Bik097 <bjs241997@gmail.com>
2019-12-17 14:47:26 -0500

    update

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ require('./models/db');
 
 const express = require('express');
 var app = express();
+const path = require('path');
+const xhb = require('express-handlebars');
 
 const studentController = require('./controllers/studentController');
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', xhb({ extname: 'hbs', defaultLayout: 'defaultLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
