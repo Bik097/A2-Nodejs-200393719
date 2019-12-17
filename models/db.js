ab89eae8d1958d31e0148bb6f7dd2e67c502426a
Bik097 <bjs241997@gmail.com>
2019-12-17 16:38:02 -0500

    test

--- models/db.js
+++ models/db.js
@@ -4,11 +4,9 @@ var url = 'mongodb+srv://bik:Test@12345@cluster0-k4bur.mongodb.net/test?retryWri
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true 
 }).then(value => console.log('Chal Pda'));
-
-//mongoose.connect('mongodb+srv://bik:Test@12345@cluster0-k4bur.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
-  //  if (!err) { console.log('MongoDB Connection Succeeded.') }
-    //else { console.log('Error in DB connection : ' + err) }
-//});
-
+// mongoose.connect('mongodb://localhost:27017/StudentDB', { useNewUrlParser: true }, (err) => {
+//     if (!err) { console.log('MongoDB Connection Succeeded.') }
+//     else { console.log('Error in DB connection : ' + err) }
+// });
 
 require('./student.model');
--- models/student.model.js
+++ models/student.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-var dataSchema = new mongoose.Schema({
+var studentSchema = new mongoose.Schema({
     fullName: {
         type: String,
         required: 'This field is required.'
@@ -17,9 +17,9 @@ var dataSchema = new mongoose.Schema({
 });
 
 // Custom validation for email
-dataSchema.path('email').validate((val) => {
+studentSchema.path('email').validate((val) => {
     emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(val);
 }, 'Invalid e-mail.');
 
-mongoose.model('list', dataSchema);
+mongoose.model('Student', studentSchema);
